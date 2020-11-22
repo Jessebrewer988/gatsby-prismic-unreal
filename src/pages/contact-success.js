89cfb826f45bf4b901f967e55fbc0e2e94977916
Jesse Brewer <jbrew119@gmail.com>
2020-11-22 16:43:14 -0500

    Made contact success page prettier

--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -2,10 +2,18 @@ import React from 'react'
 import Layout from '../components/layout';
 import styled from 'styled-components';
 
+const ContentWrapper = styled.section`
+  max-width: 800px;
+  margin: 0 auto;
+`
+
 const ContactSuccess = () => {
   return (
     <Layout>
-      <h1>Thanks for reaching out!</h1>
+      <ContentWrapper>
+        <h1>Thanks for reaching out!</h1>
+        <p>I will get back to you ASAP</p>
+      </ContentWrapper>
     </Layout>
   )
 }
